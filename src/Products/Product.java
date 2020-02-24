a202c2314cca5d5982ade975c576cbe500e414c3
1853684 <1853684@20.8.21.12>
2020-02-24 12:02:48 -0800

    Almost finished product class

--- src/Products/Product.java
+++ src/Products/Product.java
@@ -1,5 +1,32 @@
 package Products;
 
-abstract class Product {
+import java.awt.Image;
 
+abstract class Product {
+	
+	private String name;
+	private boolean onSale;
+	private int price;
+	
+	public Product(String name, boolean onSale, int price)
+	{
+		this.name = name;
+		this.onSale = onSale;
+		this.price = price;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public boolean getOnSale()
+	{
+		return onSale;
+	}
+	
+	public int getPrice()
+	{
+		return price;
+	}
 }
