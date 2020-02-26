5ed59bdb734068be112aa2968cbb90cb1593955f
AadilU <42853025+AadilU@users.noreply.github.com>
2020-02-26 11:50:50 -0800

    Update Product.java

--- src/Products/Product.java
+++ src/Products/Product.java
@@ -2,17 +2,27 @@ package Products;
 
 import java.awt.Image;
 
+import javax.swing.ImageIcon;
+
 abstract class Product {
 	
 	private String name;
 	private boolean onSale;
 	private int price;
+	public Image c;
 	
 	public Product(String name, boolean onSale, int price, String imagePath)
 	{
 		this.name = name;
 		this.onSale = onSale;
 		this.price = price;
+		initImage(imagePath);
+	}
+	
+	public void initImage(String path)
+	{
+		ImageIcon ii = new ImageIcon(path);
+		c = ii.getImage();
 	}
 	
 	public String getName()
@@ -28,6 +38,11 @@ abstract class Product {
 	public int getPrice()
 	{
 		return price;
+	}
+	
+	public Image getImage()
+	{
+		return c;
 	}
 	
 	abstract void setDimensions(String size);
