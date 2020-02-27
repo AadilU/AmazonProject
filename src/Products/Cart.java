75a56f1a406e69c397a991948af7fc30c010f165
1902566 <1902566@20.8.21.12>
2020-02-27 11:51:43 -0800

    ui update

--- src/Products/Cart.java
+++ src/Products/Cart.java
@@ -1,35 +1,28 @@
 package Products;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.BoxLayout;
-import java.awt.Button;
 import javax.swing.ImageIcon;
-import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
-public class MainUI extends JFrame {
+public class Cart extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtSearch;
-	private JMenu mnNewMenu;
 	private JMenuItem mntmHome;
-	private JMenuItem mntmItem2;
-	private JPanel categoryExplorerPanel;
-	private JLabel lblImage;
-	private JButton forwardButton;
 
 	/**
 	 * Launch the application.
@@ -38,7 +31,7 @@ public class MainUI extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainUI frame = new MainUI();
+					Cart frame = new Cart();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -50,7 +43,7 @@ public class MainUI extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MainUI() {
+	public Cart() {
 		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -60,21 +53,24 @@ public class MainUI extends JFrame {
 		menuBar.setBackground(new Color(47, 79, 79));
 		setJMenuBar(menuBar);
 		
-		mnNewMenu = new JMenu("");
-		mnNewMenu.setIcon(new ImageIcon(MainUI.class.getResource("/Images/icons8-menu-rounded-30.png")));
+		JMenu mnNewMenu = new JMenu("");
+		mnNewMenu.setIcon(new ImageIcon(Cart.class.getResource("/Images/icons8-menu-rounded-30.png")));
 		menuBar.add(mnNewMenu);
 		
+		JMenuItem mntmItem1;
 		mntmHome = new JMenuItem("Home");
-		mnNewMenu.add(mntmHome);
 		mntmHome.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
                 MainUI frame = new MainUI();
                 frame.setVisible(true);;
             }
 		});
+		mnNewMenu.add(mntmHome);
 		
-		mntmItem2 = new JMenuItem("Item 2");
+		JMenuItem mntmItem2 = new JMenuItem("Item 2");
 		mnNewMenu.add(mntmItem2);
+		
+		
 		
 		txtSearch = new JTextField();
 		txtSearch.setText("Search");
@@ -101,35 +97,7 @@ public class MainUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		forwardButton = new JButton("");
-		forwardButton.setIcon(new ImageIcon(MainUI.class.getResource("/Images/icons8-arrow-right-64.png")));
-		forwardButton.setBounds(372, 5, 73, 107);
-		contentPane.add(forwardButton);
 		
-		JButton backButton = new JButton("");
-		backButton.setBackground(Color.WHITE);
-		backButton.setIcon(new ImageIcon(MainUI.class.getResource("/Images/icons8-arrow-left-64.png")));
-		backButton.setBounds(5, 5, 73, 107);
-		contentPane.add(backButton);
 		
-		categoryExplorerPanel = new JPanel();
-		categoryExplorerPanel.setBounds(5, 5, 440, 107);
-		contentPane.add(categoryExplorerPanel);
-		
-		lblImage = new JLabel("Image");
-		categoryExplorerPanel.add(lblImage);
-		
-		JLabel lblNewLabel = new JLabel("Ad");
-		lblNewLabel.setBounds(6, 112, 142, 123);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblAd = new JLabel("Ad");
-		lblAd.setBounds(159, 112, 142, 123);
-		contentPane.add(lblAd);
-		
-		JLabel lblItem = new JLabel("Ad");
-		lblItem.setHorizontalAlignment(SwingConstants.CENTER);
-		lblItem.setBounds(303, 112, 142, 123);
-		contentPane.add(lblItem);
 	}
 }
