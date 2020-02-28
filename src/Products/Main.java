45033fac20636ec7265b8d2da05066781f9c51ad
1902566 <1902566@20.8.21.12>
2020-02-28 11:50:45 -0800

    fin

---
+++ src/Products/Main.java
@@ -0,0 +1,11 @@
+package Products;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		Shirts s = new Shirts();
+		System.out.println(s.ShirtsList.get(0).getName());
+	}
+
+}
--- src/Products/Outerwear.java
+++ src/Products/Outerwear.java
@@ -13,9 +13,9 @@ public class Outerwear{
 	
 	public void initList()
 	{
-		OuterwearList.add(new Clothing("Adidas", false, 60, ""));
-		OuterwearList.add(new Clothing("Kirklands", true, 10, ""));
-		OuterwearList.add(new Clothing("Guess", false, 50, ""));
-		OuterwearList.add(new Clothing("Goodwill Outerwear", true, 0, ""));
+		OuterwearList.add(new Clothing("Adidas", false, 60, "src/Images/didasjacket.jpg"));
+		OuterwearList.add(new Clothing("Kirklands", true, 10, "src/Images/kirk jacket.jpg"));
+		OuterwearList.add(new Clothing("Guess", false, 50, "src/Images/guessjacxket.jpg"));
+		OuterwearList.add(new Clothing("Goodwill Outerwear", true, 0, "src/Images/goodwilljacket.jpg"));
 	}
 }
